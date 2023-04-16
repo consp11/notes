1af137744ccac11045ffdaf0a125a6924c308733
fdsc <fdsc@yandex.ru>
2023-04-16 09:46:11 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/clamav-2.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/clamav-2.sh
@@ -13,6 +13,6 @@ sudo -u clamav freshclam >& /inRam/clamav.log
 
 # sudo clamscan -iozr --follow-dir-symlinks=2 --follow-file-symlinks=2 --max-filesize=256M --max-scansize=256M --max-recursion=128 --max-dir-recursion=128 --alert-encrypted=no --alert-broken=yes --pcre-match-limit=200000 --pcre-recmatch-limit=10000 --pcre-max-filesize=256M --bytecode-timeout=600000 /usr/bin /usr/sbin /usr/**/bin /usr/**/sbin /opt &>> /inRam/clamav.log
 
-sudo clamscan -iozr --follow-dir-symlinks=2 --follow-file-symlinks=2 --max-filesize=256M --max-scansize=256M --max-recursion=128 --max-dir-recursion=128 --alert-encrypted=no --alert-broken=yes --pcre-match-limit=200000 --pcre-recmatch-limit=10000 --pcre-max-filesize=256M --bytecode-timeout=600000 /usr/sbin /usr/**/bin /usr/**/sbin /opt &>> /inRam/clamav.log
+sudo nice -n 19 clamscan -iozr --follow-dir-symlinks=2 --follow-file-symlinks=2 --max-filesize=12M --max-scansize=12M --max-recursion=128 --max-dir-recursion=128 --alert-encrypted=no --alert-broken=yes --pcre-match-limit=200000 --pcre-recmatch-limit=10000 --pcre-max-filesize=12M --bytecode-timeout=600000 --scan-archive=no --scan-xmldocs=no --scan-hwp3=no --scan-html=no --scan-swf=no --scan-pdf=no --scan-pe=no --phishing-scan-urls=no --scan-mail=no --phishing-sigs=no --scan-elf=yes /usr/sbin /usr/bin /usr/**/bin /usr/**/sbin /opt &>> /inRam/clamav.log
 
 # /usr/sbin /usr/**/bin /usr/**/sbin &>> /inRam/clamav.log
