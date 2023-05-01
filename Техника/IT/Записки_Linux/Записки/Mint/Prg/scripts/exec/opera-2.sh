b9cc0cc8a71d5cad2c4bcaa828a2e0ab5819a55d
fdsc <fdsc@yandex.ru>
2023-05-01 08:44:20 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/exec/opera-2.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/exec/opera-2.sh
@@ -3,7 +3,7 @@ cd ~
 # cd /home/main/users/tor-browser
 
 # nice -n 18 opera
-sudo systemd-run --scope --nice=18 --uid=opera --gid=opera -p CPUQuota=70% -p CPUQuotaPeriodSec=1000ms opera
+sudo systemd-run --scope --nice=18 --uid=opera --gid=opera -p CPUQuota=38% -p CPUQuotaPeriodSec=1000ms opera
 
 disown -a
 
--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad.sh
@@ -11,13 +11,27 @@ nice -n 11 /usr/lib/notepadqq/notepadqq-bin &  &> /inRam/startup-nqq-s.log
 # nice -n 19 disk-wait 1
 
 # --new-window
-nice -n 11 /usr/lib/notepadqq/notepadqq-bin --new-window '/home/main/users/inet/_/keys-var/notepad.sh' '/home/main/users/inet/_/plans/Планы/Дела/--0' '/home/main/users/inet/_/plans/Планы/+' "/home/main/users/inet/_/plans/Планы/Дела/Частые" '/home/main/users/inet/_/lit/Литература/Религия/Пост/11 Немного о голодании.txt' '/home/main/users/inet/_/plans/ГолоданиеИПост/2023/Диета-апрель-дневник' '/home/main/users/inet/_/plans/ГолоданиеИПост/2023/Диета-апрель' '/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-22' '/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-26' '/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-27' &  &> /inRam/startup-nqq-pl.log
+nice -n 11 /usr/lib/notepadqq/notepadqq-bin --new-window \
+'/home/main/users/inet/_/keys-var/notepad.sh' \
+'/home/main/users/inet/_/plans/Планы/Дела/--0' \
+'/home/main/users/inet/_/plans/Планы/+' \
+"/home/main/users/inet/_/plans/Планы/Дела/Частые" \
+'/home/main/users/inet/_/lit/Литература/Религия/Пост/11 Немного о голодании.txt' '/home/main/users/inet/_/plans/ГолоданиеИПост/2023/Диета-апрель-дневник' \
+'/home/main/users/inet/_/plans/ГолоданиеИПост/2023/Диета-апрель' \
+'/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-22' \
+'/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-26' \
+'/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-27' \
+'/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/Апрель-29' \
+&  &> /inRam/startup-nqq-pl.log
 
 
 # /usr/bin/ebook-viewer '/home/main/users/inet/Docs/Сергей Лукьяненко-Собрание сочинений/Соглашение/3. Прыжок.fb2' &  &> /dev/null
 
 # Thunar '/home/main/users/inet/Docs/Звездная Елена/FB2/Долина драконов/'
-Thunar '/home/main/users/inet/Docs/Сапковский+/Анджей Сапковский - Ведьмак (сборник) (Сага о ведьмаке) - 2014/' '/home/main/users/inet/Docs/Тетлок Ф., Гарднер Д. - Думай медленно - предсказывай точно/'
+Thunar \
+'/home/main/users/inet/Docs/Сапковский+/Анджей Сапковский - Ведьмак (сборник) (Сага о ведьмаке) - 2014/' \
+'/home/main/users/inet/Docs/Тетлок Ф., Гарднер Д. - Думай медленно - предсказывай точно/' \
+&
 
 # ./_sh/kgk.sh
 
