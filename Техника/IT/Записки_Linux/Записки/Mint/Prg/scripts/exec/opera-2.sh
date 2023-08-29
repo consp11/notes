7d6e1fdcd75e4d335d63da7f9d7f58c99627e62d
fdsc <fdsc@yandex.ru>
2023-08-29 10:41:48 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/exec/opera-2.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/exec/opera-2.sh
@@ -4,9 +4,9 @@ cd ~
 # cd /home/main/users/tor-browser
 
 # nice -n 18 opera
-# percent='38%'
-percent='70%'
-sudo systemd-run --scope --nice=18 --uid=opera --gid=opera -p CPUQuota=$percent -p CPUQuotaPeriodSec=100ms opera
+percent='38%'
+# percent='70%'
+sudo systemd-run --scope --nice=18 --uid=opera --gid=opera -p CPUQuota=$percent -p CPUQuotaPeriodSec=1000ms opera
 
 
 disown -a
