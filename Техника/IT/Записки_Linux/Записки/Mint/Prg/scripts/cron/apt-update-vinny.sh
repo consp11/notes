766270d2f1147aeaf74fbe098d2f62ee122cea24
fdsc <fdsc@yandex.ru>
2023-10-04 10:14:36 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/cron/apt-update-vinny.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/cron/apt-update-vinny.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+function waitPing(i)
+{
+    # Ожидаем появления интернета
+    ping -s 16 -c 1 77.88.8.88 >> /dev/null
+    while [[ $? -ne 0 && $i -gt 0 ]]
+    do
+      sleep 5
+      i=$(($i-1))
+      ping -s 16 -c 1 77.88.8.88 >> /dev/null
+    done
+
+    ping -s 16 -c 1 77.88.8.88 &>> /inRam-Logs/upgrade
+}
+
+echo -e "\n\n\n" &>> /inRam-Logs/upgrade
+date &>> /inRam-Logs/upgrade
+waitPing 1000
+
+date &>> /inRam-Logs/upgrade
 sudo apt-get update -y &>> /inRam-Logs/upgrade
 sudo apt-get -y --with-new-pkgs --fix-broken upgrade &>> /inRam-Logs/upgrade
 
 if [[ $? -ne 0 ]]
 then
+waitPing 100
+
+date &>> /inRam-Logs/upgrade
 sudo apt-get -y --with-new-pkgs --fix-broken upgrade &>> /inRam-Logs/upgrade
 fi
 
