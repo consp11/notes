7c623355fc954b486ce05a24dd9a872b5eb27994
fdsc <fdsc@yandex.ru>
2023-12-21 10:01:23 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad.sh
@@ -22,6 +22,7 @@ nice -n 11 /usr/lib/notepadqq/notepadqq-bin --new-window \
 nice -n 11 /usr/lib/notepadqq/notepadqq-bin --new-window \
 '/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/05 Май-14-Маск' \
 '/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/09 сен-30' \
+'/Arcs/Repos/consp11.github.io/free/05/Конспекты/2023/12 дек-23' \
 '/home/main/users/inet/_/lit/Корректура/Живой доспех' \
 '/home/main/users/inet/_/lit/Корректура/Старатель1' \
 &  &> /inRam/startup-nqq-pl.log
