d465f47ec927dad10f60d3e69e5cb8e74a2a5293
fdsc <fdsc@yandex.ru>
2024-02-07 17:16:47 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad-reports.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad-reports.sh
@@ -1,6 +1,7 @@
 /usr/lib/notepadqq/notepadqq-bin --new-window '/A/service/aide/report.log' '/inRam/aide-clamav.log' '/inRam/cp-git-h.log' &  &> /inRam/startup-nqq-pl.log
 
-xfce4-terminal --tab -x watch 'cat /inRam/aide-clamav.log | tail -n 25' &
-sleep 2
-xfce4-terminal --tab -x watch 'cat /inRam/cp-git-h.log | tail -n 25' &
-xfce4-terminal --tab -x watch 'cat /inRam-Logs/upgrade | tail -n 25' &
+xfce4-terminal --tab -T 'aide-clamav.log' -x watch 'cat /inRam/aide-clamav.log | tail -n 25' &
+/home/main/bin/waitwindow.sh 'aide-clamav.log'
+
+xfce4-terminal --tab -T '/inRam/cp-git-h.log' -x watch 'cat /inRam/cp-git-h.log | tail -n 25' &
+xfce4-terminal --tab -T '/inRam-Logs/upgrade' -x watch 'cat /inRam-Logs/upgrade | tail -n 25' &
