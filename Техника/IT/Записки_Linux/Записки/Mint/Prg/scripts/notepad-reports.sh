e28b1ed06f9f70185a7067e4d70165d7bcba5ffb
fdsc <fdsc@yandex.ru>
2024-02-24 08:49:29 +0300

    automatic commit

--- Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad-reports.sh
+++ Техника/IT/Записки_Linux/Записки/Mint/Prg/scripts/notepad-reports.sh
@@ -2,7 +2,8 @@
 
 xfce4-terminal --tab -T 'aide-clamav.log' -x watch -n 16 'cat /inRam/aide-clamav.log | tail -n 25' &
 /home/main/bin/waitwindow.sh 'aide-clamav.log'
-sleep 1
+sleep 3
 
-xfce4-terminal --tab -T '/inRam/cp-git-h.log' -x watch -n 16 'cat /inRam/cp-git-h.log | tail -n 25' &
-xfce4-terminal --tab -T '/inRam-Logs/upgrade' -x watch -n 16 'cat /inRam-Logs/upgrade | tail -n 25' &
+xfce4-terminal --tab -T '/inRam/cp-git-h.log' -x watch -n 17 'cat /inRam/cp-git-h.log | tail -n 25' &
+xfce4-terminal --tab -T '/inRam-Logs/upgrade' -x watch -n 17 "cat /inRam-Logs/upgrade | tail -n 25" &
+xfce4-terminal --tab -T '/inRam-Logs/upgrade' -x watch -n 67 "cat /inRam-Logs/upgrade | egrep -i '(:$)|(^[[:blank:]])|(архивов.$)|(B\.$)|(бновлено [1-9])|(, установлено [1-9])|(отмечено [1-9])' | tail -n 25 ; echo; date" &
